c51d2feee067e2edb7ff4c15b7d69bf52fc809de
bala-amypo <bala@amypo.in>
2025-12-19 06:17:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/DuplicateRuleRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.DuplicateRule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DuplicateRuleRepository extends JpaRepository<DuplicateRule, Long> {
+
+    Optional<DuplicateRule> findByRuleName(String ruleName);
+}
---
+++ src/main/java/com/example/demo/repository/TicketRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    List<Ticket> findByUser_Id(Long id);
+
+    List<Ticket> findByCategory_Id(Long id);
+
+    List<Ticket> findByStatus(String status);
+
+    List<Ticket> findBySubjectContainingIgnoreCaseOrDescriptionContainingIgnoreCase(
+            String subject,
+            String description
+    );
+}
