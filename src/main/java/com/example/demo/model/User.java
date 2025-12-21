5fe6fab5ce6ecd534dec5c5f45e85aaff62aef2d
bala-amypo <bala@amypo.in>
2025-12-21 16:13:50 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,65 +1,69 @@
-package com.example.demo.model;
-
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(uniqueConstraints = {
-        @UniqueConstraint(columnNames = "email")
-})
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
-    private String fullName;
-
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false, length = 100)
-    private String password;
-
-    @Column(nullable = false)
-    private String role = "USER";
-
-    private LocalDateTime createdAt;
-
-    public User() {}
-
-    public User(Long id, String fullName, String email, String password, String role) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-        if (this.role == null) {
-            this.role = "USER";
-        }
-    }
-
-  
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
-
-    public LocalDateTime getCreatedAt() { return createdAt; }
-}
+package com.example.demo.model; 
+import jakarta.persistence.*; 
+import java.time.LocalDateTime; 
+import java.util.List; 
+@Entity 
+  @Table(name = "users") 
+public class User { 
+@Id 
+@GeneratedValue(strategy = GenerationType.IDENTITY) 
+  private Long id; 
+  private String fullName; 
+ @Column(unique = true) 
+   private String email; 
+   private String password; 
+private String role; 
+private LocalDateTime createdAt; 
+@OneToMany(mappedBy = "user") 
+private List<Ticket> tickets; 
+public User() { 
+} 
+public User(String fullName, String email, String password, String 
+role) { 
+this.fullName = fullName; 
+this.email = email; 
+this.password = password; 
+this.role = (role == null) ? "USER" : role; 
+} 
+public Long getId() { 
+return id; 
+} 
+public void setId(Long id) { 
+this.id = id; 
+} 
+public String getFullName() { 
+return fullName; 
+} 
+public void setFullName(String fullName) { 
+this.fullName = fullName; 
+} 
+public String getEmail() { 
+return email; 
+} 
+public void setEmail(String email) { 
+this.email = email; 
+} 
+public String getPassword() { 
+return password; 
+} 
+public void setPassword(String password) { 
+this.password = password; 
+} 
+public String getRole() { 
+return role; 
+} 
+public void setRole(String role) { 
+this.role = role; 
+} 
+public LocalDateTime getCreatedAt() { 
+return createdAt; 
+} 
+public void setCreatedAt(LocalDateTime createdAt) { 
+this.createdAt = createdAt; 
+} 
+public List<Ticket> getTickets() { 
+return tickets; 
+} 
+public void setTickets(List<Ticket> tickets) { 
+this.tickets = tickets; 
+} 
