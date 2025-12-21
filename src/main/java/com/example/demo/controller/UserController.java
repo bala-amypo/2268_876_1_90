f1b8a0cd61f2449f7fb99e61084955f5fbd2cf61
bala-amypo <bala@amypo.in>
2025-12-21 17:16:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,28 +1,53 @@
-package com.example.demo.controller; 
-import com.example.demo.model.User; 
-import com.example.demo.service.UserService; 
-import io.swagger.v3.oas.annotations.tags.Tag; 
-import org.springframework.http.ResponseEntity; 
-import org.springframework.web.bind.annotation.*; 
-import java.util.List; 
-@RestController 
-@RequestMapping("/api/users") 
-@Tag(name = "User Controller", description = "Manage users") 
-public class UserController { 
-private final UserService userService; 
-public UserController(UserService userService) { 
-this.userService = userService; 
-} 
-@PostMapping("/register") 
-public ResponseEntity<User> registerUser(@RequestBody User user) { 
-return ResponseEntity.ok(userService.registerUser(user)); 
-} 
-@GetMapping("/all") 
-public ResponseEntity<List<User>> getAllUsers() { 
-return ResponseEntity.ok(userService.getAllUsers()); 
-} 
-@GetMapping("/{id}") 
-public ResponseEntity<User> getUser(@PathVariable Long id) { 
-return ResponseEntity.ok(userService.getUser(id)); 
-} 
-} 
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@Tag(
+        name = "User Controller",
+        description = "Manage users"
+)
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(
+            UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(
+            @RequestBody User user) {
+
+        return ResponseEntity.ok(
+                userService.registerUser(user)
+        );
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        return ResponseEntity.ok(
+                userService.getAllUsers()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                userService.getUser(id)
+        );
+    }
+}
