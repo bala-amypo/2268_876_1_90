aa1fe8001c6f383e91256437f068013bd05960b5
bala-amypo <bala@amypo.in>
2025-12-24 04:49:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,13 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// public class HomeController {
+@RestController
+public class HomeController {
 
-//     @GetMapping("/")
-//     public String home() {
-//         return "Application is running successfully!";
-//     }
-// }
+    @GetMapping("/")
+    public String home() {
+        return "Application is running successfully!";
+    }
+}
